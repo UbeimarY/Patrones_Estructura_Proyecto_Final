55b11ddec07c89dca743fccc8014ea987c1325c9
KevinMartinez <kevin.martinezandr@campusucc.edu.co>
2025-04-29 17:42:57 -0500

    Correcion de errores al crear un Proyecto Java - Maven

---
+++ backend/training/src/main/java/com/cognitiveapp/ApplicationTraining.java
@@ -0,0 +1,11 @@
+package com.cognitiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTraining {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTraining.class, args);
+    }
+}
