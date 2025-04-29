ddb19e896774b682331ae37e68d32ca0d59a7346
Camilo Toro <camilotoro@email.edu.co>
2025-04-29 17:52:10 -0500

    Prueba Unitaria Mvn (Test)

---
+++ backend/training/src/test/java/com/cognitiveapp/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognitiveapp.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
