f08fee1a09fe0760a72df6edea581d76d7330f31
KevinMartinez <kevin.martinezandr@campusucc.edu.co>
2025-04-29 18:06:32 -0500

    Agregando directorio de estructura de datos

---
+++ backend/training/src/main/java/com/cognitiveapp/datastructures/MyDynamicArray.java
@@ -0,0 +1,46 @@
+package com.cognitiveapp.training.datastructures;
+
+@SuppressWarnings("unchecked")
+public class MyDynamicArray<T> {
+    private T[] data;
+    private int size;
+
+    public MyDynamicArray() {
+        data = (T[]) new Object[10];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return data[index];
+    }
+
+    public void add(T element) {
+        if (size == data.length) {
+            resize();
+        }
+        data[size++] = element;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        T removed = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i+1];
+        }
+        data[--size] = null;
+        return removed;
+    }
+
+    private void resize() {
+        T[] newData = (T[]) new Object[data.length * 2];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+}
