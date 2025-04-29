8aee613e6b70195269357763f6088828c4db1ded
Ubeimar Yepes <ubeimar.yepespor@campusucc.edu.co>
2025-04-29 18:10:15 -0500

    Modificando contenidos y agregando Directorio repositorio

---
+++ backend/training/src/main/java/com/cognitiveapp/training/repository/GameRepository.java
@@ -0,0 +1,10 @@
+package com.cognitiveapp.training.repository;
+
+import com.cognitiveapp.training.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+    // Métodos personalizados si fueran necesarios
+}
---
+++ backend/training/src/main/java/com/cognitiveapp/training/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cognitiveapp.training.repository;
+
+import com.cognitiveapp.training.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    AppUser findByUsername(String username);
+}
